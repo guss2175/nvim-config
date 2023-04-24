27aa3934cfe3edec90c3caf3069ed4da710d437d
laipmv-2881 <91654423+laipmv-2881@users.noreply.github.com>
2023-04-24 17:09:31 +0700

    Init

---
+++ init.lua
@@ -0,0 +1,4 @@
+require("base")
+require("user.themes.gruvbox")
+require("user.plugins_register")
+require("user.plugins.nvim_tree")
---
+++ lua/base.lua
@@ -0,0 +1,3 @@
+vim.opt.number = true -- set line numbers for pairing
+vim.opt.syntax = "on" -- enable syntax highlighting
+vim.opt.termguicolors = true -- enable 24-bit RGB colors
